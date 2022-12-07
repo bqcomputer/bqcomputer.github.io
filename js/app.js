7958a38d9e7a87d57f40e90144662cde1153d03e
bqcomputer <bqcomgroup@gmail.com>
2022-12-07 20:55:21 +0700

    Saving to Github

--- js/app.js
+++ js/app.js
@@ -2,6 +2,28 @@
 AOS.init();
 
 // NAVBAR
-$(window).scroll(function () {
-  $("nav").toggleClass("scrolled", $(this).scrollTop() > 50);
+const nav_toggler = document.querySelector(".header .nav-bar .nav-list .nav-toggler");
+const mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
+const menu_item = document.querySelectorAll(".header .nav-bar .nav-list ul li a");
+const header = document.querySelector(".header.container");
+
+nav_toggler.addEventListener("click", () => {
+  nav_toggler.classList.toggle("active");
+  mobile_menu.classList.toggle("active");
+});
+
+document.addEventListener("scroll", () => {
+  let scroll_position = window.scrollY;
+  if (scroll_position > 250) {
+    header.style.backgroundColor = "#29323c";
+  } else {
+    header.style.backgroundColor = "transparent";
+  }
+});
+
+menu_item.forEach((item) => {
+  item.addEventListener("click", () => {
+    nav_toggler.classList.toggle("active");
+    mobile_menu.classList.toggle("active");
+  });
 });
