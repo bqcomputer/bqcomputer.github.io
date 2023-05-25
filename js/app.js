fcafa2898a0cf6258b2545a6cd6620fb2c62bfbd
bqcomputer <bqcomgroup@gmail.com>
2023-05-25 20:08:09 +0700

    Saving to github

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // AOS
 AOS.init();
 
@@ -35,41 +34,3 @@ $(".read-button").on("click", function () {
   let replaceText = $(this).parent().hasClass("showContent") ? "< Kembali" : "Selengkapnya...";
   $(this).text(replaceText);
 });
-=======
-// AOS
-AOS.init();
-
-// NAVBAR
-const nav_toggler = document.querySelector(".header .nav-bar .nav-list .nav-toggler");
-const mobile_menu = document.querySelector(".header .nav-bar .nav-list ul");
-const menu_item = document.querySelectorAll(".header .nav-bar .nav-list ul li a");
-const header = document.querySelector(".header.container");
-
-nav_toggler.addEventListener("click", () => {
-  nav_toggler.classList.toggle("active");
-  mobile_menu.classList.toggle("active");
-});
-
-document.addEventListener("scroll", () => {
-  let scroll_position = window.scrollY;
-  if (scroll_position > 250) {
-    header.style.backgroundColor = "#29323c";
-  } else {
-    header.style.backgroundColor = "transparent";
-  }
-});
-
-menu_item.forEach((item) => {
-  item.addEventListener("click", () => {
-    nav_toggler.classList.toggle("active");
-    mobile_menu.classList.toggle("active");
-  });
-});
-
-// LEARN
-$(".read-button").on("click", function () {
-  $(this).parent().toggleClass("showContent");
-  let replaceText = $(this).parent().hasClass("showContent") ? "< Kembali" : "Selengkapnya...";
-  $(this).text(replaceText);
-});
->>>>>>> d39ebb5 (first commit)
